a86f9d7724d204d32bc478032fcba3d9f63bf4f2
Anton Kolotaev <anton.kolotaev@gmail.com>
2016-08-29 13:51:56 +0200

    removing QuickSort

--- src/main/scala/sorting/QuickSort.scala
+++ src/main/scala/sorting/QuickSort.scala
@@ -2,32 +2,6 @@ package sorting
 
 object QuickSort {
 
-
-    /**
-     * Quick sort strategy
-     * @param partition -- function that accepts a subrange in an array (array, lo, hi) and partitions it somehow
-     *                     it returns index 'p' that forms two ranges:
-     *                      [lo, p) is less than some pivot
-     *                      [p, hi) is greater or equal to the pivot
-     * @param array - an array to be sorted in-place
-     * @tparam T - a type that supports comparisons
-     * @return - nothing since the array is sorted in place
-     */
-    def apply[T : Ordering](partition : (Array[T], Int, Int, Ordering[T]) => (Int,Int))
-                           (array : Array[T]) : Unit =
-    {
-        def impl(lo : Int, hi : Int) : Unit =
-        {
-            if (lo < hi) {
-                val (lo_to, hi_from) = partition(array, lo, hi, implicitly[Ordering[T]])
-                impl(lo, lo_to)
-                impl(hi_from, hi)
-            }
-        }
-
-        impl(0, array.length)
-    }
-
     /**
      * Partitions somehow array[lo until hi] on two parts: the left one is less than 'p' and the right one is greater or equal to 'p'
      * @param array - array to be partitioned
